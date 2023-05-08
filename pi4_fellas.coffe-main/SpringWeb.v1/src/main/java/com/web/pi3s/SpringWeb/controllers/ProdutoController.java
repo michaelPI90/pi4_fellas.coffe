b467dbfda52d8dfeb13309b7b092ae0aa101571b
michaelPI90 <michaellima760@gmail.com>
2023-05-08 18:48:13 -0300

    mostrando img do banco

--- pi4_fellas.coffe-main/SpringWeb.v1/src/main/java/com/web/pi3s/SpringWeb/controllers/ProdutoController.java
+++ pi4_fellas.coffe-main/SpringWeb.v1/src/main/java/com/web/pi3s/SpringWeb/controllers/ProdutoController.java
@@ -12,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.Model;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 import com.web.pi3s.SpringWeb.models.Imagenmodels;
 import com.web.pi3s.SpringWeb.models.Produtomodels;
 import com.web.pi3s.SpringWeb.repositorio.Imagenrespo;
@@ -69,40 +71,35 @@ public class ProdutoController {
     public ModelAndView cadastro(@ModelAttribute("produto") Produtomodels produtos, Imagenmodels novaImagem,
             @RequestParam("imagens") MultipartFile[] imagens) throws IOException {
 
-       // List<byte[]> imagensBytes = new ArrayList<>();
+        // List<byte[]> imagensBytes = new ArrayList<>();
 
-       for (int i = 0; i < imagens.length; i++) {
-           
+        for (int i = 0; i < imagens.length; i++) {
+
             novaImagem.setNomeImagem(imagens[i].getOriginalFilename());
-           
+
             novaImagem.setConteudoImagem(imagens[i].getBytes());
             novaImagem.setProduto(produtos);
             produtos.setImagem(imagens[i].getBytes());
-          
-         
+
         }
-       
+
         produtorespo.save(produtos);
         imagenrespo.save(novaImagem);
-     
 
         ModelAndView mv = new ModelAndView("produtos/produtos");
         mv.addObject("produtos", produtos);
 
-       
         return listaProduto(mv);
 
     }
 
-    @GetMapping("/product/{productId}")
-public String showProductPage(Model model, @PathVariable Integer productId) {
-    Optional<Imagenmodels> imageList =  imagenrespo.findById(productId);
-    model.addAttribute("imageList", imageList);
+    // @GetMapping("/product/{productId}")
+    // public String showProductPage(Model model, @PathVariable Integer productId) {
+    //     Optional<Imagenmodels> imageList = imagenrespo.findById(productId);
+    //     model.addAttribute("imageList", imageList);
 
-    return "/mostrar/teste";
-}
-
-
+    //     return "/mostrar/teste";
+    // }
 
     @GetMapping("/ConsultarProduto")
     public String consultar(Model model, @RequestParam String nomeProduto) {
@@ -133,6 +130,18 @@ public String showProductPage(Model model, @PathVariable Integer productId) {
 
     }
 
+    @GetMapping("/product/{id}")
+    public ResponseEntity<byte[]> exibirImagem(@PathVariable Long id) {
+        Optional<Imagenmodels> imagemOptional = imagenrespo.findById(id);
+
+        if (imagemOptional.isPresent()) {
+            Imagenmodels imagem = imagemOptional.get();
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imagem.getConteudoImagem());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/salvarProduto")
     public ModelAndView salvarProduto(Model model, Integer userID, Produtomodels product) {
@@ -157,15 +166,15 @@ public String showProductPage(Model model, @PathVariable Integer productId) {
         System.out.println("================BATENDO AQUI NO METODO DE ALTERAR PRODUTO===================");
         System.out.println("PRODUTO: " + produto);
         ResponseEntity resp;
-        try{
+        try {
             produtorespo.alterarProduto(produto.getQntdEstoque(), produto.getId());
             resp = ResponseEntity.ok("Produto alterado com sucesso!");
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            resp = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Não foi possível alterar o produto: " + e.getMessage());
+            resp = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Não foi possível alterar o produto: " + e.getMessage());
         }
         return resp;
     }
-
 
 }
