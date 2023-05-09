7dcee3fb6f7d811596bfdff1a3501567ef9db7b2
michaelPI90 <michaellima760@gmail.com>
2023-05-08 22:10:47 -0300

    imagens lazy

--- pi4_fellas.coffe-main/SpringWeb.v1/src/main/java/com/web/pi3s/SpringWeb/models/Imagenmodels.java
+++ pi4_fellas.coffe-main/SpringWeb.v1/src/main/java/com/web/pi3s/SpringWeb/models/Imagenmodels.java
@@ -27,7 +27,7 @@ public class Imagenmodels {
     private String nomeImagem;
 
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tb_produto_id")
     private Produtomodels produto;
 
