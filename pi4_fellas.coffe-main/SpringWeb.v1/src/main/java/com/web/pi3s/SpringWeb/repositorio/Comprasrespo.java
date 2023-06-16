d9d7a3e9fb6d9e7a7df71a1daa281a517312e99b
michaelPI90 <michaellima760@gmail.com>
2023-06-16 11:52:13 -0300

    TelaBackoffice

--- pi4_fellas.coffe-main/SpringWeb.v1/src/main/java/com/web/pi3s/SpringWeb/repositorio/Comprasrespo.java
+++ pi4_fellas.coffe-main/SpringWeb.v1/src/main/java/com/web/pi3s/SpringWeb/repositorio/Comprasrespo.java
@@ -1,22 +1,25 @@
 package com.web.pi3s.SpringWeb.repositorio;
 
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.web.pi3s.SpringWeb.models.Clientemodels;
 import com.web.pi3s.SpringWeb.models.Compra;
 import com.web.pi3s.SpringWeb.models.Produtomodels;
 
 import java.util.List;
+import java.util.Optional;
+@Repository
+public interface Comprasrespo extends JpaRepository<Compra, Long> {
 
+    Compra findByCliente(Clientemodels cliente);
 
-public interface Comprasrespo extends  CrudRepository<Compra, Integer>  {
+    @Query(value = "select * from tb_compra where cliente_id = ? order by data_compra desc", nativeQuery = true)
+    public List<Compra> ordernarPedidos(int id);
 
-Compra findByCliente(Clientemodels cliente);
+    Compra findById(int id);
 
-@Query(value="select * from tb_compra where cliente_id = ? order by data_compra desc", nativeQuery = true)
-public List<Compra> ordernarPedidos(int id);
-
-Compra findById(int id);
+    Optional<Compra> findBynumeroPedido(String numeroPedido);
 }
